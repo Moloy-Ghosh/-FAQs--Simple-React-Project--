9d30533286074fae8a7441f4f7a440fda6b46b0a
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-02-27 14:26:28 +0600

    Add files via upload

---
+++ Original code form src folder/data.js
@@ -0,0 +1,27 @@
+export const faqsData=[
+    {
+        id:1,
+        title:"I want to know about you!",
+        desc:"Ok fine. My name is Moloy Kumar Ghosh. I am a student of BSc in CSE. I am now learning computer languages. In future I want to get a job as a web developer or softwere engineer."
+    },
+    {
+        id:2,
+        title:"How can we reach you?",
+        desc:"You can call me. (+8801780303302) or you can email me (moloy.ghosh.cse@gmail.com)"
+    },
+    {
+        id:3,
+        title:"Do you want to work with us?",
+        desc:"Yes. I want."
+    },
+    {
+        id:4,
+        title:"What type of work you like?",
+        desc:"Web development or coding or programming related."
+    },
+    {
+        id:5,
+        title:"Can you work hard?",
+        desc:"Yes! I can. Also I can learn a new programming laguage so quickly if it necessery for work."
+    },
+]
